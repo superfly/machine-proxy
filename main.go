ff705a03fa5b667e6d2de00b374b6da2c64e1515
Joshua Sierles <joshua@hey.com>
2021-11-04 11:24:54 +0100

    simplify

--- main.go
+++ main.go
@@ -5,21 +5,7 @@ import (
 	"io"
 	"log"
 	"net/http"
-	"strings"
 )
-
-// Hop-by-hop headers. These are removed when sent to the backend.
-// http://www.w3.org/Protocols/rfc2616/rfc2616-sec13.html
-var hopHeaders = []string{
-	"Connection",
-	"Keep-Alive",
-	"Proxy-Authenticate",
-	"Proxy-Authorization",
-	"Te", // canonicalized version of "TE"
-	"Trailers",
-	"Transfer-Encoding",
-	"Upgrade",
-}
 
 func copyHeader(dst, src http.Header) {
 	for k, vv := range src {
@@ -27,22 +13,6 @@ func copyHeader(dst, src http.Header) {
 			dst.Add(k, v)
 		}
 	}
-}
-
-func delHopHeaders(header http.Header) {
-	for _, h := range hopHeaders {
-		header.Del(h)
-	}
-}
-
-func appendHostToXForwardHeader(header http.Header, host string) {
-	// If we aren't the first proxy retain prior
-	// X-Forwarded-For information as a comma+space
-	// separated list and fold multiple headers into one.
-	if prior, ok := header["X-Forwarded-For"]; ok {
-		host = strings.Join(prior, ", ") + ", " + host
-	}
-	header.Set("X-Forwarded-For", host)
 }
 
 type proxy struct {
@@ -62,8 +32,6 @@ func (p *proxy) ServeHTTP(wr http.ResponseWriter, req *http.Request) {
 
 	client := &http.Client{}
 
-	delHopHeaders(req.Header)
-
 	resp, err := client.Do(req)
 	if err != nil {
 		http.Error(wr, "Server Error", http.StatusInternalServerError)
@@ -72,8 +40,6 @@ func (p *proxy) ServeHTTP(wr http.ResponseWriter, req *http.Request) {
 	defer resp.Body.Close()
 
 	log.Println(req.RemoteAddr, " ", resp.Status)
-
-	delHopHeaders(resp.Header)
 
 	copyHeader(wr.Header(), resp.Header)
 	wr.WriteHeader(resp.StatusCode)
